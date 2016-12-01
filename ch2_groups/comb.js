f6d20b790e7969b2fe96d7d8d6ed4bd01702265e
Kevin Kerr <kkerr@triplelift.com>
2016-12-01 10:27:12 -0500

    calculate factorial

--- ch2_groups/comb.js
+++ ch2_groups/comb.js
@@ -1,8 +1,31 @@
-var itoa = String.fromCharCode;
-var initCharCode = 'a'.charCodeAt(0);
-var i = 0;
-var ALPHA_LIMIT = 26;
-while (i<ALPHA_LIMIT) {
-    console.log(i,itoa(initCharCode+i));
-    i++;
-}
+var nameFunction = (function () { 
+    var itoa = String.fromCharCode;
+    var initCharCode = 'a'.charCodeAt(0);
+    var i = 0;
+    var ALPHA_LIMIT = 26;
+    return function (f) {
+        if (i >= ALPHA_LIMIT) {
+            throw new Error();
+        }
+        return {name:itoa(initCharCode+i++),func:f};
+    }
+})();
+
+var fact = function (n) {
+    var p = 1;
+    while (n > 0) {
+        p*=n;
+        n--;
+    }
+    return p;
+};
+
+var getSymmetricSet = function (n) {
+    var s = [];
+    var i = n;
+    while (i > 0) {
+        s.push(i);
+        i--;
+    }
+
+};
